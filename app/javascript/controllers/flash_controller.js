5b19dca1ef3896186bcc9fbf97d6d71488aca097
MacDaniel Maisel <macdanielrmaisel@gmail.com>
2022-03-31 08:02:41 -0700

    Added linter feedback.

--- app/javascript/controllers/flash_controller.js
+++ app/javascript/controllers/flash_controller.js
@@ -1,25 +1,26 @@
 import { Controller } from '@hotwired/stimulus';
 
 export default class AdminFlashController extends Controller {
-    static targets = [ "close" ]
-    setCookie() {
-      const flashData = this.closeTarget
-      const messageId = flashData.dataset.id;
-      const messageExpiration = flashData.dataset.expire;
-      this.setAdminFlashCookie(messageId, 'disabled', messageExpiration);
-    }
 
-    setAdminFlashCookie(name, value, date) {
-      const cookieDate = this.convertToCookieDate(date);
-      document.cookie = `admin_flash_${name}=${value}; expires=${cookieDate}; path=/`
-    }
+  static targets = ['close']
 
-    convertToCookieDate(date) {
-      const parts = date.split('-');
-      const day = parts[2].split(' ');
-      const jsDate = new Date(parts[0], parts[1] - 1, day[0]);
-      const cookieDate = jsDate.toUTCString();
-      return cookieDate
-    }
+  setCookie() {
+    const flashData = this.closeTarget;
+    const messageId = flashData.dataset.id;
+    const messageExpiration = flashData.dataset.expire;
+    this.setAdminFlashCookie(messageId, 'disabled', messageExpiration);
+  }
 
+  setAdminFlashCookie(name, value, date) {
+    const cookieDate = this.convertToCookieDate(date);
+    document.cookie = `admin_flash_${name}=${value}; expires=${cookieDate}; path=/`
+  }
+
+  convertToCookieDate(date) {
+    const parts = date.split('-');
+    const day = parts[2].split(' ');
+    const jsDate = new Date(parts[0], parts[1] - 1, day[0]);
+    const cookieDate = jsDate.toUTCString();
+    return cookieDate;
+  }     
 }
